d8d19bfbc4d0cd9cb4b9c4a8216d9aa8450020ed
PavelK6896 <43148722+PavelK6896@users.noreply.github.com>
2020-05-31 20:20:18 +0300

    "annotation mapping, static html read, file send"

--- src/main/java/pavserk/MainServlet.java
+++ src/main/java/pavserk/MainServlet.java
@@ -2,11 +2,16 @@ package pavserk;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Map;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
+@WebServlet(urlPatterns = {"/cool", "/ser/*"})
 public class MainServlet extends HttpServlet {
     @Override
     public void init(ServletConfig config) throws ServletException {
@@ -18,12 +23,32 @@ public class MainServlet extends HttpServlet {
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.getWriter().write("service start Text =)\n");
         super.service(req, resp);
-        resp.getWriter().write("service end Text =)\n");
+        resp.getWriter().write("service end Text =) HEEEEEEEEER!!\n ");
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().write("doGet Text =)\n");
+        String uri = req.getRequestURI();
+        String params = formatParams(req);
+        resp.getWriter().write("doGet Text =)\nURI: " + uri + "\nParams:\n" + params + "\n");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String uri = req.getRequestURI();
+        String params = formatParams(req);
+        resp.getWriter().write("doPost Text =)\nURI: " + uri + "\nParams:\n" + params + "\n");
+    }
+
+    private String formatParams(HttpServletRequest req) {
+        return req.getParameterMap()
+                .entrySet()
+                .stream()
+                .map(entry -> {
+                    String param = String.join(" and ", entry.getValue());
+                    return entry.getKey() + " => " + param;
+                })
+                .collect(Collectors.joining("\n"));
     }
 
     @Override
